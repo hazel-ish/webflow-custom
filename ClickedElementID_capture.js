a5b9013e558d8778bf534a024071ac8e47207656
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-15 19:18:22 +0530

    Update ClickedElementID_capture.js

--- ClickedElementID_capture.js
+++ ClickedElementID_capture.js
@@ -1,5 +1,4 @@
 document.addEventListener('click', function(event) {
-console.log("CDN link working");
 var clickedElement=event.target;
 var clickedElementId = event.target.id;
 if (clickedElementId) {
