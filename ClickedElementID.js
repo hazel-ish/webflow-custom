07e5f1c452ae8aad3a67fea7ab9301ed2c4fe9bb
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-15 18:06:13 +0530

    Delete ClickedElementID.js

--- ClickedElementID.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function(event) {
-console.log("CDN link working");
-var clickedElement=event.target;
-var clickedElementId = event.target.id;
-if (clickedElementId) {
-localStorage.setItem('clickedKey', clickedElementId);}});
