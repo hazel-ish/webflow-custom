22e50634e3e106f0b59cced697a738d27f6bf7c6
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-15 22:43:41 +0530

    Update ClickedElementID_capture.js

--- ClickedElementID_capture.js
+++ ClickedElementID_capture.js
@@ -2,7 +2,9 @@ document.addEventListener('click', function(event) {
     var clickedElements = JSON.parse(localStorage.getItem('clickedKey')) || [];
     var clickedElementId = event.target.id;
     if (clickedElementId) {
+        console.log('clickedElement',clickedElementId);
         clickedElements.push(clickedElementId);
         localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
+        console.log('Value set in local storage');
     }
 });
