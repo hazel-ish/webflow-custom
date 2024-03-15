9df2232dc1f78a4f73f1924870a55177fbad5474
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-15 23:59:48 +0530

    Update ClickedElementID_capture.js

--- ClickedElementID_capture.js
+++ ClickedElementID_capture.js
@@ -7,10 +7,18 @@ document.addEventListener('click', function(event) {
         clickedElements=JSON.parse(clickedElements);
     }
     var clickedElementId = event.target.id;
+    var clickedElementAttr= event.target.getAttribute('blog-card-defi-module');
     if (clickedElementId) {
         console.log('clickedElement',clickedElementId);
         if(clickedElementId!=='marketing-2' && clickedElementId!=='analytics-2' && clickedElementId!=='personalization-2' && clickedElementId!=='w-dropdown-toggle-0')
         clickedElements.push(clickedElementId);
+        localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
+        console.log('Value set in local storage');
+    }
+    if (clickedElementAttr) {
+        console.log('clickedElementAttr',clickedElementAttr);
+        if(clickedElementAttr!=='marketing-2' && clickedElementAttr!=='analytics-2' && clickedElementAttr!=='personalization-2' && clickedElementAttr!=='w-dropdown-toggle-0')
+        clickedElements.push(clickedElementAttr);
         localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
         console.log('Value set in local storage');
     }
