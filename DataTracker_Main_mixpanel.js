95ce8a4c13917e9b6441dfb7c632ca991c713eb3
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-15 18:07:49 +0530

    Add files via upload

---
+++ DataTracker_Main_mixpanel.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+$(document).on('click', '[data-tracker]', function(e) {
+    var trackData = $(this).data('tracker');
+    if (!trackData) { return; }
+    var tagData = ParseTagData.tagData(trackData);
+    if (!tagData.action || !tagData.label ) { return; }
+    Track.trackEvent('click', { action: tagData.action , label:  tagData.label });});
+var ParseTagData = { tagData : function ( data ) {
+    var tmpData = data.split("|");
+    if (tmpData.length !=2 ) { return ""; }
+    return { "action": tmpData[0] ,"label": tmpData[1] };}};
+var Track= { trackEvent: function (eventType, data) {mixpanel.track( data.action );}};});
