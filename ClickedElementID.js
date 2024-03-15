dbf2e119c699998792449693f558f521bca9d330
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-15 17:42:49 +0530

    Add files via upload
    This code gets the clicked element's ID from previous page which led to the current page.

---
+++ ClickedElementID.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('click', function(event) {
+    var clickedElement=event.target;
+var clickedElementId = event.target.id;
+if (clickedElementId) {
+    localStorage.setItem('clickedKey', clickedElementId);}});
